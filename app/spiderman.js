c3ae9f86f6485052c0d6c0ec902023d2b0228352
FernandoGR <gamboa_029@hotmail.com>
2022-04-24 18:44:33 -0500

    P2W3V3

--- app/spiderman.js
+++ app/spiderman.js
@@ -1,3 +1,14 @@
-class Spiderman{}
+class Spiderman{
+
+    constructor(name, edad, actor, peliculas, cine){
+        //la etiqueta que se le pone, debe ser igual al que se use en test.js
+        this.name = name
+        this.edad = edad
+        this.actor = actor
+        this.peliculas = peliculas
+        this.cine = cine
+    }
+
+}
 
 module.exports = Spiderman
